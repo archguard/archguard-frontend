e15ecefd6489ea62c2e0618c96973165bcbf8999
hang.liu <hang.liu@thoughtworks.com>
2020-07-16 11:24:41 +0800

    fix: remove redux with DependenceConfig

--- dependence_visual/src/pages/analysis/dependence/DependenceConfig/DependenceConfig.jsx
+++ dependence_visual/src/pages/analysis/dependence/DependenceConfig/DependenceConfig.jsx
@@ -1,56 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMount } from "react-use";
+import _ from "lodash";
 import { Card, Drawer } from "antd";
-import { connect } from "react-redux";
 import ConfigBox from "@/components/ConfigBox";
-import { createConfig, updateConfig, deleteConfig } from "@/api/dependence/dependenceConfig";
+import {
+  queryConfig,
+  createConfig,
+  updateConfig,
+  deleteConfig,
+} from "@/api/dependence/dependenceConfig";
 import { configType } from "./config";
-import { resetConfig } from "../actions/config";
-import { hiddenConfig } from "../actions/configVisible";
 
-// import "../../../mock/config";
+import "@/mock/config";
 
-class Config extends React.Component {
-  render() {
-    return (
-      <Drawer
-        title="配置"
-        placement="right"
-        width="50%"
-        visible={this.props.configVisible}
-        onClose={() => this.props.hiddenConfig()}
-      >
-        <div>
-          {configType.map((item) => {
-            return (
-              <Card title={item.label} style={{ marginBottom: "20px" }} key={item.type}>
-                <ConfigBox
-                  label={item.label}
-                  formItems={item.formItems}
-                  data={this.props.config[item.type]}
-                  createItem={createConfig}
-                  updateItem={updateConfig}
-                  deleteItem={deleteConfig}
-                  resetConfigs={(configData) => this.props.resetConfig(configData)}
-                />
-              </Card>
-            );
-          })}
-        </div>
-      </Drawer>
-    );
-  }
+function DependenceConfig(props) {
+  const { visible, hide } = props;
+
+  const [configData, setConfigData] = useState({});
+
+  useMount(() => {
+    queryConfig().then((res) => {
+      setConfigData(_.groupBy(res, (item) => item.type));
+    });
+  });
+
+  return (
+    <Drawer title="配置" placement="right" width="50%" visible={visible} onClose={() => hide()}>
+      <div>
+        {configType.map((item) => {
+          return (
+            <Card title={item.label} style={{ marginBottom: "20px" }} key={item.type}>
+              <ConfigBox
+                label={item.label}
+                formItems={item.formItems}
+                data={configData[item.type]}
+                createItem={createConfig}
+                updateItem={updateConfig}
+                deleteItem={deleteConfig}
+              />
+            </Card>
+          );
+        })}
+      </div>
+    </Drawer>
+  );
 }
 
-function mapStateToProps(state) {
-  return {
-    config: state.dependence.config,
-    configVisible: state.dependence.configVisible,
-  };
-}
-
-const mapDispatchToProps = {
-  resetConfig,
-  hiddenConfig,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Config);
+export default DependenceConfig;
