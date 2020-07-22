bb51c3fd99e7ebc2e45a44c48184f2890e8384e7
na.luo <na.luo@thoughtworks.com>
2020-07-22 16:12:33 +0800

    [luona] project info add username password field

--- dependence_visual/src/pages/Home/components/ProjectInfo/index.tsx
+++ dependence_visual/src/pages/Home/components/ProjectInfo/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Col, Input, notification, Row } from "antd";
+import { Button, Input, notification } from "antd";
 import Select from "antd/es/select";
 import React from "react";
 import {
@@ -6,6 +6,7 @@ import {
   queryProjectInfo,
   updateProjectInfo,
 } from "../../../../api/addition/projectInfo";
+import './index.less'
 
 export default class ProjectInfo extends React.Component {
   constructor(props) {
@@ -14,12 +15,24 @@ export default class ProjectInfo extends React.Component {
       projectName: "",
       repo: [],
       repoType: "",
+      username: '',
+      password: '',
+      _password: '',
     };
+  }
+
+  get passwordChanged() {
+    return this.state._password !== this.state.password
+  }
+
+  cpPassword (password: string) {
+    this.setState({ _password: password })
   }
 
   componentDidMount() {
     queryProjectInfo().then((res) => {
-      this.setState(res);
+      this.setState({ ...res });
+      this.cpPassword(res.password)
     });
   }
 
@@ -32,54 +45,62 @@ export default class ProjectInfo extends React.Component {
     if (!Array.isArray(request.repo)) {
       request.repo = request.repo.split(",");
     }
-    if (this.state.id) {
-      updateProjectInfo(this.state).then((res) => {
-        if (res.success) {
-          notification.success({
-            message: res.message,
-          });
-        } else {
-          notification.error({
-            message: res.message,
-          });
-        }
-      });
+
+    const { id, projectName, repo, repoType, username } = request
+    const password = this.passwordChanged ? request.password : undefined
+
+    if (id) {
+      updateProjectInfo({ id, projectName, repo, repoType, username, password })
+        .then((res) => {
+          if (res.success) {
+            this.cpPassword(request.password)
+            notification.success({
+              message: res.message,
+            });
+          } else {
+            notification.error({
+              message: res.message,
+            });
+          }
+        });
     } else {
-      createProjectInfo(this.state).then((res) => {
-        if (res.success) {
-          notification.success({
-            message: res.message,
-          });
-          this.setState({ id: res.id });
-        } else {
-          notification.error({
-            message: res.message,
-          });
-        }
-      });
+      createProjectInfo({ projectName, repo, repoType, username, password })
+        .then((res) => {
+          if (res.success) {
+            this.cpPassword(request.password)
+            notification.success({
+              message: res.message,
+            });
+            this.setState({ id: res.id });
+          } else {
+            notification.error({
+              message: res.message,
+            });
+          }
+        });
     }
   }
 
   render() {
     return (
-      <div>
-        <Row gutter={16} align="middle" style={{ marginBottom: "8px" }}>
-          <Col span={2}>
-            <span>项目名称：</span>
-          </Col>
-          <Col span={6}>
+      <div className="project-info">
+        <div className="project-info-row">
+          <div className="label">
+            <span>项目名称</span>
+          </div>
+          <div className="content">
             <Input
               placeholder="项目名称"
               value={this.state.projectName}
               onChange={(e) => this.onChang("projectName", e.target.value)}
             ></Input>
-          </Col>
-        </Row>
-        <Row gutter={16} align="middle" style={{ marginBottom: "8px" }}>
-          <Col span={2}>
-            <span>仓库类型：</span>
-          </Col>
-          <Col span={6}>
+          </div>
+        </div>
+        <div className="project-info-row">
+          <div className="label">
+            <span>仓库类型</span>
+          </div>
+          <div className="content">
             <Select
               placeholder="仓库类型"
               value={this.state.repoType}
@@ -88,32 +109,56 @@ export default class ProjectInfo extends React.Component {
               <Select.Option value={"GIT"}>GIT</Select.Option>
               <Select.Option value={"SVN"}>SVN</Select.Option>
             </Select>
-          </Col>
-        </Row>
-        <Row gutter={16} align="middle" style={{ marginBottom: "8px" }}>
-          <Col span={2}>
-            <span>仓库地址：</span>
-          </Col>
-          <Col span={22}>
+          </div>
+        </div>
+        <div className="project-info-row">
+          <div className="label">
+            <span>仓库地址</span>
+          </div>
+          <div className="content">
             <Input
               placeholder="仓库地址"
               value={this.state.repo}
               onChange={(e) => this.onChang("repo", e.target.value)}
             ></Input>
-          </Col>
-        </Row>
-        {/* <Row gutter={16} align="middle" style={{ marginBottom: "8px" }}>
-          <Col span={2}>
-            <span>数据库表：</span>
-          </Col>
-          <Col span={22}>
-            <TextArea
+          </div>
+        </div>
+        <div className="project-info-row">
+          <div className="label">
+            <span>用户名</span>
+          </div>
+          <div className="content">
+            <Input
+              placeholder="用户名"
+              value={this.state.username}
+              onChange={(e) => this.onChang("username", e.target.value)}
+            ></Input>
+          </div>
+        </div>
+        <div className="project-info-row">
+          <div className="label">
+            <span>密码</span>
+          </div>
+          <div className="content">
+            <Input
+              placeholder="密码"
+              value={this.state.password}
+              onChange={(e) => this.onChang("password", e.target.value)}
+            ></Input>
+          </div>
+        </div>
+        {/* <div className="project-info-row">
+          <div className="label">
+            <span>数据库表</span>
+          </div>
+          <div className="content">
+          <TextArea
               placeholder="数据库表文件内容"
               value={this.state.sql}
               onChange={(e) => this.onChang("sql", e.target.value)}
             ></TextArea>
-          </Col>
-        </Row> */}
+          </div>
+        </div> */}
         <Button onClick={() => this.onSaveClick()} style={{ float: "right" }} type="primary">
           保存
         </Button>
