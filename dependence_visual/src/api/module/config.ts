54d50b1c6e1b993710906f9ec3484f4ada07ea4a
qinjialei <qinjialei@foxmail.com>
2020-09-04 15:31:00 +0800

    add axiosWithModule func

--- dependence_visual/src/api/module/config.ts
+++ dependence_visual/src/api/module/config.ts
@@ -1,4 +1,7 @@
-import { storage } from '@/store/storage/sessionStorage'
+import { storage } from '@/store/storage/sessionStorage';
+import { axiosWithBaseURL } from '../axios';
 
-const systemId = storage.getSystemId()
-export const baseURL = `/api/module/systems/${systemId}`
+const systemId = storage.getSystemId();
+export const baseURL = `/api/module/systems/${systemId}`;
+export const axiosWithModule = axiosWithBaseURL(baseURL);
+
