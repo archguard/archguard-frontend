384948357e990e28dceda068dee3a3ed511c737d
qinjialei <qinjialei@foxmail.com>
2020-09-07 17:58:16 +0800

    use different color by category

--- dependence_visual/src/pages/QualityEvaluation/QualityEvaluation.tsx
+++ dependence_visual/src/pages/QualityEvaluation/QualityEvaluation.tsx
@@ -2,19 +2,34 @@ import React from "react";
 import { Chart, Tooltip, Interval, Coordinate, Interaction } from "bizcharts";
 
 const data = [
-  { badSmell: '体量过大-类较大', category: '体量过大', count: 20 },
-  { badSmell: '设计冗余1', category: '设计冗余', count: 28.8 },
-  { badSmell: '过高耦合1', category: '过高耦合', count: 39.3 },
-  { badSmell: '过低内聚1', category: '过低内聚', count: 81.4 },
-  { badSmell: '过于复杂1', category: '过于复杂', count: 47 },
-  { badSmell: '缺乏分层1', category: '缺乏分层', count: 20.3 },
-  { badSmell: '体量过大2', category: '体量过大', count: 10 },
-  { badSmell: '设计冗余2', category: '设计冗余', count: 23.2 },
-  { badSmell: '过高耦合2', category: '过高耦合', count: 34.5 },
-  { badSmell: '过低内聚2', category: '过低内聚', count: 99.7 },
-  { badSmell: '过于复杂2', category: '过于复杂', count: 52.6 },
-  { badSmell: '缺乏分层2', category: '缺乏分层', count: 35.5 },
+  { badSmell: "体量过大-类较大", category: "体量过大", count: 20 },
+  { badSmell: "体量过大-类较大2", category: "体量过大", count: 10 },
+  { badSmell: "体量过大-类较大3", category: "体量过大", count: 10 },
+  { badSmell: "设计冗余1", category: "设计冗余", count: 28.8 },
+  { badSmell: "过高耦合1", category: "过高耦合", count: 39.3 },
+  { badSmell: "过低内聚1", category: "过低内聚", count: 21.4 },
+  { badSmell: "过于复杂1", category: "过于复杂", count: 47 },
+  { badSmell: "缺乏分层1", category: "缺乏分层", count: 20.3 },
+  { badSmell: "体量过大2", category: "体量过大", count: 10 },
+  { badSmell: "设计冗余2", category: "设计冗余", count: 23.2 },
+  { badSmell: "过高耦合2", category: "过高耦合", count: 34.5 },
+  { badSmell: "过低内聚2", category: "过低内聚", count: 19.7 },
+  { badSmell: "过于复杂2", category: "过于复杂", count: 22.6 },
+  { badSmell: "缺乏分层2", category: "缺乏分层", count: 35.5 },
 ];
+
+function getColor(category: string): string {
+  const colorMap = {
+    体量过大: "#ee8572",
+    设计冗余: "#3aafae",
+    过高耦合: "#4d64b5",
+    过低内聚: "#c06c9f",
+    过于复杂: "#d98e37",
+    缺乏分层: "#546b66",
+  } as any;
+
+  return colorMap[category];
+}
 
 function QualityEvaluation() {
   return (
@@ -26,7 +41,7 @@ function QualityEvaluation() {
             marginRatio: 1,
           },
         ]}
-        color="badSmell"
+        color={["badSmell*category", (xVal, category) => getColor(category)]}
         position="category*count"
       />
       <Coordinate type="polar" />
