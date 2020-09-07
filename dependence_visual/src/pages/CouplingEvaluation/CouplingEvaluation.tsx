4c94354fb526c71e4f60302bb5860f9fc61ccfcd
na.luo <na.luo@thoughtworks.com>
2020-09-07 10:30:56 +0800

    tabs issues

--- dependence_visual/src/pages/CouplingEvaluation/CouplingEvaluation.tsx
+++ dependence_visual/src/pages/CouplingEvaluation/CouplingEvaluation.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import IssuesList from '../SizingEvaluation/components/IssuesList'
 import { IssuesConfigs } from '../SizingEvaluation/components/IssuesConfig.config'
+import { Tabs } from 'antd'
 
 const CouplingEvaluation = () => {
   return (
-    <div>
-      <IssuesList issuesConfig={IssuesConfigs.HUB} />
-    </div>
+    <Tabs onChange={() => {}} type="card">
+      <Tabs.TabPane tab="枢纽模块" key="hub">
+        <IssuesList issuesConfig={IssuesConfigs.HUB} />
+      </Tabs.TabPane>
+    </Tabs>
   )
 }
 
--- dependence_visual/src/pages/SizingEvaluation/SizingEvaluation.tsx
+++ dependence_visual/src/pages/SizingEvaluation/SizingEvaluation.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import IssuesList from './components/IssuesList'
 import { IssuesConfigs } from './components/IssuesConfig.config'
+import { Tabs } from 'antd'
 
 const SizingEvaluation = () => {
   return (
-    <div>
-      <IssuesList issuesConfig={IssuesConfigs.METHOD} />
-      <IssuesList issuesConfig={IssuesConfigs.CLASS} />
-    </div>
+    <Tabs onChange={() => {}} type="card">
+      <Tabs.TabPane tab="过大的方法" key="method">
+        <IssuesList issuesConfig={IssuesConfigs.METHOD} />
+      </Tabs.TabPane>
+      <Tabs.TabPane tab="过大的类" key="class">
+        <IssuesList issuesConfig={IssuesConfigs.CLASS} />
+      </Tabs.TabPane>
+    </Tabs>
   )
 }
 
