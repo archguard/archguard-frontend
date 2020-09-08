1cdd522a3f120c2d795439f4493dc9438b07c194
qinjialei <qinjialei@foxmail.com>
2020-09-08 14:22:40 +0800

    fix Summary layout

--- dependence_visual/src/pages/Summary/Summary.tsx
+++ dependence_visual/src/pages/Summary/Summary.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Summary.less";
-
-import { Chart, Tooltip, Interval, Coordinate, Interaction, Legend } from "bizcharts";
 import QualityEvaluation from "./components/QualityEvaluation";
+import { XButton } from "@/components/Button/Button";
+import { getOverview, Overview } from "@/api/module/overview";
 
 function Summary() {
+  const [summary, setSummary] = useState<Overview[]>([]);
+
+  useEffect(() => {
+    getOverview().then((res) => {
+      setSummary(res.data);
+    });
+  }, []);
+
   return (
     <div>
-      <div className={styles.header}>111111111</div>
+      <div className={styles.header}>
+        <div className={styles.title}>
+          <div className={styles.name}>架构质量评估</div>
+          <div className={styles.date}>15Jul2020</div>
+        </div>
+        <XButton>下载详细报告</XButton>
+      </div>
+
       <div className={styles.body}>
-        <QualityEvaluation />
+        <QualityEvaluation data={summary} />
       </div>
     </div>
   );
