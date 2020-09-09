5b1a9d7e429c732897d091f41cb86484ac0c362d
qinjialei <qinjialei@foxmail.com>
2020-09-09 22:16:41 +0800

    delete api file overview

--- dependence_visual/src/api/module.ts
+++ dependence_visual/src/api/module.ts
@@ -1,13 +1,18 @@
 import { storage } from '@/store/storage/sessionStorage';
 import { useGet } from './axios';
-import { Overview } from './module/overview';
 
 const systemId = storage.getSystemId();
 export const baseURL = `/api/module/systems/${systemId}`;
 
+interface Overview {
+  badSmell: string,
+  category: string,
+  count: number,
+}
 interface UseOverview {
   data: Overview[];
 }
+
 export function useOverview() {
   const { data, run } = useGet<UseOverview>(`${baseURL}/overview`);
   return {
