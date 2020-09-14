49138f389a5168c60e5c52881c722f4342668375
qinjialei <qinjialei@foxmail.com>
2020-09-14 16:27:58 +0800

    add Card component

--- dependence_visual/src/components/Basic/Card/Card.tsx
+++ dependence_visual/src/components/Basic/Card/Card.tsx
@@ -2,16 +2,13 @@ import React from "react";
 import './Card.less'
 
 interface BaCardProps {
-  children?: React.ReactChildren;
-  header: () => React.ReactElement;
+  children?: React.ReactNode;
 }
 
 export const BaCard = (props: BaCardProps) => {
-  const { header } = props;
   return (
     <div className="BaCard">
-      <div className="BaCard-header">{header()}</div>
-      <div className="BaCard-body">1123</div>
+      {props.children}
     </div>
   );
 };
