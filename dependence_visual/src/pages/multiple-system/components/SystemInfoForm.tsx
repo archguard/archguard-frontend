da661c8fc6d3739865a730e715b3a2b87695e369
na.luo <na.luo@thoughtworks.com>
2020-09-14 15:39:53 +0800

    validator duplicate system name

--- dependence_visual/src/pages/multiple-system/components/SystemInfoForm.tsx
+++ dependence_visual/src/pages/multiple-system/components/SystemInfoForm.tsx
@@ -1,36 +1,43 @@
-import React, { forwardRef, useImperativeHandle, useEffect } from 'react'
-import { Form, Input, Select, Button } from 'antd';
-import { useForm } from 'antd/lib/form/Form';
-import { Store } from 'antd/lib/form/interface';
-import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
-import { SystemInfo } from '@/api/addition/systemInfo';
+import React, { forwardRef, useImperativeHandle, useEffect } from "react";
+import { Form, Input, Select, Button } from "antd";
+import { useForm } from "antd/lib/form/Form";
+import { Store } from "antd/lib/form/interface";
+import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
+import { SystemInfo } from "@/api/addition/systemInfo";
+import useSystemList from "@/store/global-cache-state/useSystemList";
 
 interface SystemInfoFormProps {
   data?: SystemInfo;
   onSubmit(systemInfo: SystemInfo): void;
 }
 const SystemInfoForm = (props: SystemInfoFormProps, ref: any) => {
-  const { data, onSubmit } = props
-  const [form] = useForm()
+  const { data, onSubmit } = props;
+  const [form] = useForm();
+  const [systemList] = useSystemList();
+  const systemNames = systemList?.value!.map((item) => item.systemName);
 
   useEffect(() => {
-    console.log(data)
-    form.setFieldsValue(data as Store)
-  }, [data])
+    form.setFieldsValue(data as Store);
+  }, [data]);
 
   useImperativeHandle(ref, () => ({
     submit: () => form.submit(),
-    clear: () => form.resetFields(
-      ['systemName', 'repoType', 'username', 'password', 'repo'])
-  }))
+    clear: () => form.resetFields(["systemName", "repoType", "username", "password", "repo"]),
+  }));
 
   const onFinish = (values: Store) => {
-    onSubmit(Object.assign({ ...data, ...values }))
-  }
+    onSubmit(Object.assign({ ...data, ...values }));
+  };
 
   const isValidUrl = (value: string) => {
-    return /^(https?:\/\/)([0-9a-z.]+)(:[0-9]+)?([/0-9a-z.]+)?(\?[0-9a-z&=]+)?(#[0-9-a-z]+)?/i.test(value)
-  }
+    return /^(https?:\/\/)([0-9a-z.]+)(:[0-9]+)?([/0-9a-z.]+)?(\?[0-9a-z&=]+)?(#[0-9-a-z]+)?/i.test(
+      value,
+    );
+  };
+
+  const isDuplicateSystemName = (systemName: string) => {
+    return systemNames?.includes(systemName);
+  };
 
   return (
     <div className="system-info-form">
@@ -39,37 +46,44 @@ const SystemInfoForm = (props: SystemInfoFormProps, ref: any) => {
         form={form}
         layout="vertical"
         onFinish={onFinish}
-        initialValues={{ repoType: 'GIT', repo: ["www"] }}>
+        initialValues={{ repoType: "GIT", repo: ["www"] }}
+      >
         <Form.Item
           name="systemName"
           label="系统名称"
-          rules={[{ required: true, message: '请输入系统名称！' }]}
-          required>
+          rules={[
+            {
+              required: true,
+              validator: (_, value) => {
+                if (!value) return Promise.reject("请输入系统名称！");
+                return isDuplicateSystemName(value)
+                  ? Promise.reject("系统名称不能重复！")
+                  : Promise.resolve();
+              },
+            },
+          ]}
+          required
+        >
           <Input placeholder="请输入系统名称" />
         </Form.Item>
         <Form.Item
           name="repoType"
           label="仓库类型"
-          rules={[{ required: true, message: '请选择仓库类型！' }]}
-          required>
+          rules={[{ required: true, message: "请选择仓库类型！" }]}
+          required
+        >
           <Select>
-            {['GIT', 'SVN'].map((value) => (
-              <Select.Option
-                value={value}
-                key={value}>
+            {["GIT", "SVN"].map((value) => (
+              <Select.Option value={value} key={value}>
                 {value}
               </Select.Option>
             ))}
           </Select>
         </Form.Item>
-        <Form.Item
-          name="username"
-          label="仓库用户名">
+        <Form.Item name="username" label="仓库用户名">
           <Input placeholder="请输入用户名" />
         </Form.Item>
-        <Form.Item
-          name="password"
-          label="仓库密码">
+        <Form.Item name="password" label="仓库密码">
           <Input.Password placeholder="请输入密码" />
         </Form.Item>
         <Form.List name="repo">
@@ -77,51 +91,64 @@ const SystemInfoForm = (props: SystemInfoFormProps, ref: any) => {
             return (
               <div>
                 <div style={{ paddingBottom: 8 }}>
-                  <span style={{
-                    marginRight: 4,
-                    color: '#ff4d4f',
-                    fontSize: 14,
-                    fontFamily: 'SimSun, sans-serif',
-                  }}>*</span>
+                  <span
+                    style={{
+                      marginRight: 4,
+                      color: "#ff4d4f",
+                      fontSize: 14,
+                      fontFamily: "SimSun, sans-serif",
+                    }}
+                  >
+                    *
+                  </span>
                   <span>仓库地址</span>
                 </div>
-                {fields.map(field => (
-                  <div style={{ display: 'flex' }} key={field.key}>
+                {fields.map((field) => (
+                  <div style={{ display: "flex" }} key={field.key}>
                     <Form.Item
                       {...field}
-                      rules={[{
-                        required: true,
-                        message: '请输入正确的仓库地址！',
-                        validator: (_, value) =>
-                          isValidUrl(value) ?
-                          Promise.resolve() :
-                          Promise.reject('请输入正确的仓库地址！'),
-                      }]}
-                      style={{ width: '100%' }}
-                      required>
-                      <Input
-                        style={{ width: '100%' }}
-                        placeholder="请输入仓库地址" />
+                      rules={[
+                        {
+                          required: true,
+                          validator: (_, value) => {
+                            if (!value) return Promise.reject("请输入仓库地址！");
+                            return isValidUrl(value)
+                              ? Promise.resolve()
+                              : Promise.reject("请输入正确的仓库地址！");
+                          },
+                        },
+                      ]}
+                      style={{ width: "100%" }}
+                      required
+                    >
+                      <Input style={{ width: "100%" }} placeholder="请输入仓库地址" />
                     </Form.Item>
-                    { fields.length > 1 ?
+                    {fields.length > 1 ? (
                       <MinusCircleOutlined
-                        style={{ margin: '0 8px', lineHeight: '32px' }}
-                        onClick={() => { remove(field.name) }}
-                      /> : null
-                    }
+                        style={{ margin: "0 8px", lineHeight: "32px" }}
+                        onClick={() => {
+                          remove(field.name);
+                        }}
+                      />
+                    ) : null}
                   </div>
-              ))}
-              <Form.Item>
-                <Button type="dashed" onClick={() => { add() }}>
-                  <PlusOutlined /> 添加仓库地址
-                </Button>
-              </Form.Item>
-            </div>
+                ))}
+                <Form.Item>
+                  <Button
+                    type="dashed"
+                    onClick={() => {
+                      add();
+                    }}
+                  >
+                    <PlusOutlined /> 添加仓库地址
+                  </Button>
+                </Form.Item>
+              </div>
             );
           }}
         </Form.List>
       </Form>
     </div>
-  )
-}
+  );
+};
 export default forwardRef(SystemInfoForm);
--- dependence_visual/src/pages/multiple-system/index.tsx
+++ dependence_visual/src/pages/multiple-system/index.tsx
@@ -1,144 +1,152 @@
-import './index.less'
-import React, { useState, useRef, useEffect } from 'react'
-import { Tabs, Row, Col, Modal, notification } from 'antd'
-import { useMount, useInterval } from 'react-use'
-import { UpOutlined } from '@ant-design/icons'
-import { scanDependence } from '@/api/scanner/dependenceScanner'
-import { SystemInfo, createSystemInfo, updateSystemInfo, deleteSystem } from '@/api/addition/systemInfo'
-import { storage } from '@/store/storage/sessionStorage'
-import useSystemList from '@/store/global-cache-state/useSystemList'
-import SystemCard from './components/SystemCard'
-import SystemInfoForm from './components/SystemInfoForm';
+import "./index.less";
+import React, { useState, useRef, useEffect } from "react";
+import { Tabs, Row, Col, Modal, notification } from "antd";
+import { useMount, useInterval } from "react-use";
+import { UpOutlined } from "@ant-design/icons";
+import { scanDependence } from "@/api/scanner/dependenceScanner";
+import {
+  SystemInfo,
+  createSystemInfo,
+  updateSystemInfo,
+  deleteSystem,
+} from "@/api/addition/systemInfo";
+import { storage } from "@/store/storage/sessionStorage";
+import useSystemList from "@/store/global-cache-state/useSystemList";
+import SystemCard from "./components/SystemCard";
+import SystemInfoForm from "./components/SystemInfoForm";
 
 interface UserProfile {
   name?: string;
   account?: string;
 }
 
-const DEFAULT_LOAD_DATA_INTERVAL = 1000 * 60 * 5
+const DEFAULT_LOAD_DATA_INTERVAL = 1000 * 60 * 5;
 
 const MultipleSystem = () => {
-  const ref = useRef<any>({})
-  const [user, setUser] = useState<UserProfile>()
-  const [systemList, load] = useSystemList()
-  const [systemInfoList, setSystemInfoList] = useState<SystemInfo[]>([])
-  const [modalVisible, setModalVisible] = useState(false)
-  const [currentSystemInfo, setCurrentSystemInfo] = useState<SystemInfo>()
+  const ref = useRef<any>({});
+  const [user, setUser] = useState<UserProfile>();
+  const [systemList, load] = useSystemList();
+  const [systemInfoList, setSystemInfoList] = useState<SystemInfo[]>([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [currentSystemInfo, setCurrentSystemInfo] = useState<SystemInfo>();
 
   useEffect(() => {
-    setSystemInfoList(systemList?.value || [])
-  }, [systemList])
+    setSystemInfoList(systemList?.value || []);
+  }, [systemList]);
 
   useMount(() => {
-    storage.setSystemId(undefined)
-    setUser({ name: '张扬', account: 'Zhang102' })
-  })
+    storage.setSystemId(undefined);
+    setUser({ name: "张扬", account: "Zhang102" });
+  });
 
-  useInterval(() => { load() }, DEFAULT_LOAD_DATA_INTERVAL)
+  useInterval(() => {
+    load();
+  }, DEFAULT_LOAD_DATA_INTERVAL);
 
   const routeToHome = (systemInfo: SystemInfo) => {
-    if (systemInfo.scanned !== "SCANNED") return
-    const { id } = systemInfo
-    storage.setSystemId(id)
-    window.location.href = `/${id}/analysis/dependence`
-  }
+    if (systemInfo.scanned !== "SCANNED") return;
+    const { id } = systemInfo;
+    storage.setSystemId(id);
+    window.location.href = `/${id}/analysis/dependence`;
+  };
 
   const onSubmitSystemInfo = (systemInfo: SystemInfo) => {
     if (systemInfo.id) {
       updateSystemInfo(systemInfo).then(() => {
         notification.success({
-          type: 'success',
-          message: '系统信息修改成功！'
-        })
-        onCancel()
-        load()
-      })
+          type: "success",
+          message: "系统信息修改成功！",
+        });
+        onCancel();
+        load();
+      });
     } else {
       createSystemInfo(systemInfo).then(() => {
         notification.success({
-          type: 'success',
-          message: '系统创建成功！'
-        })
-        onCancel()
-        load()
-      })
+          type: "success",
+          message: "系统创建成功！",
+        });
+        onCancel();
+        load();
+      });
     }
-  }
+  };
 
   const onCreateClick = () => {
-    setCurrentSystemInfo(undefined)
-    setModalVisible(true)
-  }
+    setCurrentSystemInfo(undefined);
+    setModalVisible(true);
+  };
 
   const onEditClick = (systemInfo: SystemInfo) => {
-    setCurrentSystemInfo({ ...systemInfo })
-    setModalVisible(true)
-  }
+    setCurrentSystemInfo({ ...systemInfo });
+    setModalVisible(true);
+  };
 
   const onRemoveClick = (systemInfo: SystemInfo) => {
     Modal.confirm({
-      type: 'error',
+      type: "error",
       title: "删除",
       content: `确定要删除 ${systemInfo.systemName} 系统吗？`,
       centered: true,
       onOk: () => {
         deleteSystem(systemInfo.id).then(() => {
-          setSystemInfoList(systemInfoList.filter((item) => item.id !== systemInfo.id))
-        })
-      }
+          setSystemInfoList(systemInfoList.filter((item) => item.id !== systemInfo.id));
+        });
+      },
     });
-  }
+  };
 
   const onSubmit = () => {
-    ref.current.submit()
-  }
+    ref.current.submit();
+  };
   const onCancel = () => {
-    setModalVisible(false)
-    ref.current.clear()
-  }
+    setModalVisible(false);
+    ref.current.clear();
+  };
 
   const onScanning = (id: number) => {
     scanDependence(id).then(() => {
-      load()
-    })
-  }
+      load();
+    });
+  };
 
   return (
     <div className="multiple-system-container">
       <div className="multiple-system-header">
         <div className="header-logo">
-          <img src={require('@/assets/Logo.png')} alt="logo"></img>
+          <img src={require("@/assets/Logo.png")} alt="logo"></img>
         </div>
-        { user &&
+        {user && (
           <div className="header-user">
-            <img src={require('@/assets/user-profile.png')}></img>
-            <span className="user-name">{user.name} / {user.account}</span>
+            <img src={require("@/assets/user-profile.png")}></img>
+            <span className="user-name">
+              {user.name} / {user.account}
+            </span>
             <UpOutlined className="user-icon" />
           </div>
-        }
+        )}
       </div>
       <div className="multiple-system-selector">
         <Tabs defaultActiveKey="my-system">
           <Tabs.TabPane tab="我的系统" key="my-system">
             <Row gutter={[12, 12]}>
               <Col xs={24} sm={12} md={8} lg={6} xxl={4}>
-                <SystemCard
-                  onClick={onCreateClick}></SystemCard>
+                <SystemCard onClick={onCreateClick}></SystemCard>
               </Col>
-              { systemInfoList.map((systemInfo) => (
+              {systemInfoList.map((systemInfo) => (
                 <Col xs={24} sm={12} md={8} lg={6} xxl={4} key={systemInfo.id}>
                   <SystemCard
                     systemInfo={systemInfo}
                     onClick={() => routeToHome(systemInfo)}
                     onScanning={() => onScanning(systemInfo.id)}
                     onEdit={() => onEditClick(systemInfo)}
-                    onRemove={() => onRemoveClick(systemInfo)}></SystemCard>
+                    onRemove={() => onRemoveClick(systemInfo)}
+                  ></SystemCard>
                 </Col>
               ))}
             </Row>
           </Tabs.TabPane>
-          <Tabs.TabPane tab="其他系统" key="other-system" disabled>
-          </Tabs.TabPane>
+          <Tabs.TabPane tab="其他系统" key="other-system" disabled></Tabs.TabPane>
         </Tabs>
       </div>
       <Modal
@@ -146,14 +154,16 @@ const MultipleSystem = () => {
         maskClosable={false}
         visible={modalVisible}
         onCancel={onCancel}
-        onOk={onSubmit}>
+        onOk={onSubmit}
+      >
         <SystemInfoForm
           ref={ref}
           data={currentSystemInfo}
-          onSubmit={onSubmitSystemInfo}></SystemInfoForm>
+          onSubmit={onSubmitSystemInfo}
+        ></SystemInfoForm>
       </Modal>
     </div>
-  )
-}
+  );
+};
 
 export default MultipleSystem;
