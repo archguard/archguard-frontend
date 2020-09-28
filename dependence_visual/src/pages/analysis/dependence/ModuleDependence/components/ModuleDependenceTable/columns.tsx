5af5f8d942479bce396296de2e5b2c61819ccc44
na.luo <na.luo@thoughtworks.com>
2020-09-28 16:43:46 +0800

    fix table route search error

--- dependence_visual/src/pages/analysis/dependence/ModuleDependence/components/ModuleDependenceTable/columns.tsx
+++ dependence_visual/src/pages/analysis/dependence/ModuleDependence/components/ModuleDependenceTable/columns.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Highlighter from 'react-highlight-words';
+import Highlighter from "react-highlight-words";
 import { Link } from "react-router-dom";
 import { Tooltip, Input, Space, Button } from "antd";
-import { SearchOutlined } from '@ant-design/icons';
-import * as _ from 'lodash'
-import { FilterDropdownProps, Key } from 'antd/lib/table/interface';
-import { storage } from '@/store/storage/sessionStorage'
+import { SearchOutlined } from "@ant-design/icons";
+import * as _ from "lodash";
+import { FilterDropdownProps, Key } from "antd/lib/table/interface";
+import { storage } from "@/store/storage/sessionStorage";
 
 export type methodDependency = {
   caller: method;
@@ -29,30 +29,38 @@ type method = {
   returnType: string;
 };
 
-let searchText: Key = ''
-let searchedColumn: string = ''
-let searchInput: Input | null = null
+let searchText: Key = "";
+let searchedColumn: string = "";
+let searchInput: Input | null = null;
 
 const getEndString = (value?: string): string => {
-  if (!value) return ''
-  return value.split(".").slice(-1).toString()
-}
+  if (!value) return "";
+  return value.split(".").slice(-1).toString();
+};
 
-const systemId = storage.getSystemId()
+const systemId = storage.getSystemId();
 
 const getColumnSearchProps = (
   title: string,
   dataIndex: string | string[],
-  renderCallback: (text: string, record: methodDependency, isHighlight: boolean) => void) => ({
-  filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }: FilterDropdownProps) => (
+  renderCallback: (text: string, record: methodDependency, isHighlight: boolean) => void,
+) => ({
+  filterDropdown: ({
+    setSelectedKeys,
+    selectedKeys,
+    confirm,
+    clearFilters,
+  }: FilterDropdownProps) => (
     <div style={{ padding: 8 }}>
       <Input
-        ref={node => { searchInput = node }}
+        ref={(node) => {
+          searchInput = node;
+        }}
         placeholder={`search ${title}`}
         value={selectedKeys[0]}
-        onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+        onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
         onPressEnter={() => handleSearch(selectedKeys, confirm, title)}
-        style={{ width: 188, marginBottom: 8, display: 'block' }}
+        style={{ width: 188, marginBottom: 8, display: "block" }}
       />
       <Space>
         <Button
@@ -70,15 +78,17 @@ const getColumnSearchProps = (
       </Space>
     </div>
   ),
-  filterIcon: (filtered: boolean) => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+  filterIcon: (filtered: boolean) => (
+    <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
+  ),
   onFilter: (value: string, record: any) => {
-    const label = _.get(record, dataIndex)
+    const label = _.get(record, dataIndex);
     const isIncludes = (label: string, value: string) => {
       if (label && value) {
-        return label.toLowerCase().includes(value.toLowerCase())
+        return label.toLowerCase().includes(value.toLowerCase());
       }
-      return false
-    }
+      return false;
+    };
     return isIncludes(label, value);
   },
   onFilterDropdownVisibleChange: (visible: boolean) => {
@@ -86,29 +96,31 @@ const getColumnSearchProps = (
       setTimeout(() => searchInput?.select());
     }
   },
-  render: (text: string, record: methodDependency) => renderCallback(text, record, searchedColumn === title)
+  render: (text: string, record: methodDependency) =>
+    renderCallback(text, record, searchedColumn === title),
 });
 
 const handleSearch = (selectedKeys: Key[], confirm: () => void, dataIndex: string) => {
   confirm();
-  searchText = selectedKeys[0]
-  searchedColumn = dataIndex
+  searchText = selectedKeys[0];
+  searchedColumn = dataIndex;
 };
 
 const handleReset = (clearFilters: () => void) => {
   clearFilters();
-  searchText = ''
-  searchedColumn = ''
-  searchInput = null
+  searchText = "";
+  searchedColumn = "";
+  searchInput = null;
 };
 
 const renderHighlighter = (text: string) => (
   <Highlighter
-    highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+    highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
     searchWords={[searchText.toString()]}
     autoEscape
     textToHighlight={getEndString(text)}
-  />)
+  />
+);
 
 const classColumn = (dataIndex: string) => {
   const classColumnRender = (text: string, record: methodDependency, isHighlight: boolean) => {
@@ -117,8 +129,9 @@ const classColumn = (dataIndex: string) => {
       <Tooltip title={text}>
         <Link
           to={{
-            pathname: `/${systemId}/analysis/dependence/class`,
+            pathname: `/${systemId}/analysis/dependence`,
             search:
+              "tab=class" +
               "className=" +
               text +
               "&dependenceType=dependencies" +
@@ -129,12 +142,12 @@ const classColumn = (dataIndex: string) => {
           {isHighlight ? renderHighlighter(text) : getEndString(text)}
         </Link>
       </Tooltip>
-    )
-  }
+    );
+  };
   const column = {
     title: "class",
     dataIndex: [dataIndex, "clazz", "name"],
-    ...getColumnSearchProps('class', [dataIndex, 'clazz', 'name'], classColumnRender),
+    ...getColumnSearchProps("class", [dataIndex, "clazz", "name"], classColumnRender),
   };
   return column;
 };
@@ -145,8 +158,9 @@ const methodColumn = (dataIndex: string) => {
     return (
       <Link
         to={{
-          pathname: `/${systemId}/analysis/dependence/method`,
+          pathname: `/${systemId}/analysis/dependence`,
           search:
+            "tab=method" +
             "className=" +
             method.clazz.name +
             "&methodName=" +
@@ -159,11 +173,11 @@ const methodColumn = (dataIndex: string) => {
         {isHighlight ? renderHighlighter(text) : getEndString(text)}
       </Link>
     );
-  }
+  };
   const column = {
     title: "method",
     dataIndex: [dataIndex, "name"],
-    ...getColumnSearchProps('method', [dataIndex, 'name'], methodColumnRender)
+    ...getColumnSearchProps("method", [dataIndex, "name"], methodColumnRender),
   };
   return column;
 };
@@ -173,7 +187,7 @@ const argumentColumn = (dataIndex: String) => {
     title: "argument",
     dataIndex: [dataIndex, "argumentTypes"],
     render: (text: string[]) => {
-      return text.map((arg) => arg.split(".").slice(-1)).join(', ');
+      return text.map((arg) => arg.split(".").slice(-1)).join(", ");
     },
   };
   return column;
