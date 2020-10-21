fe04429bac9184631eb86d32157073ce3e3833b1
Xiaolin <xlbian@thoughtworks.com>
2020-10-21 09:47:39 +0800

    #555 Use libarary moment instead of origin date format to format date.

--- dependence_visual/src/pages/multipleSystem/components/SystemCard.tsx
+++ dependence_visual/src/pages/multipleSystem/components/SystemCard.tsx
@@ -3,6 +3,7 @@ import { Card, Button, Dropdown, Menu } from "antd";
 import Meta from "antd/lib/card/Meta";
 import { PlusOutlined, EllipsisOutlined } from "@ant-design/icons";
 import { SystemInfo } from "@/api/addition/systemInfo";
+import moment from 'moment';
 
 interface SystemCardProps {
   systemInfo?: SystemInfo;
@@ -64,11 +65,6 @@ const SystemCard = (props: SystemCardProps) => {
           );
   };
 
-  const recentScannedTime = (systemInfo: SystemInfo) => {
-    const updatedTime = new Date(systemInfo.updatedTime).toLocaleString('en-US', { hour12: false });
-    return updatedTime;
-  }
-
   return systemInfo ? (
     <Card hoverable className="multiple-system-card">
       <div className="multiple-system-card-content">
@@ -89,7 +85,7 @@ const SystemCard = (props: SystemCardProps) => {
         <div className="card-btn">{renderSystemButton(systemInfo)}</div>
       </div>
       <div className="multiple-system-card-title">
-        <Meta title={systemInfo.systemName} description={`最近扫描时间: ${recentScannedTime(systemInfo)}`} />
+        <Meta title={systemInfo.systemName} description={`最近扫描时间: ${moment(systemInfo.updatedTime).format('DD/MM/YYYY HH:mm')}`} />
       </div>
     </Card>
   ) : (
