98b6f9d81d92acacf6bc787d1e0900b10914ff2c
Xiaolin <xlbian@thoughtworks.com>
2020-11-04 16:36:04 +0800

    #565 sidebar坏味道阈值切接口和可选编辑, step2, 添加重置和确定按钮，实现其功能，把Collapse包裹进Form中，以便可以正常提交表单内容。

--- dependence_visual/src/pages/systemEvolving/BadSmellThreshold/BadSmellThreshold.tsx
+++ dependence_visual/src/pages/systemEvolving/BadSmellThreshold/BadSmellThreshold.tsx
@@ -1,15 +1,25 @@
+import { updateSystemInfo } from '@/api/addition/systemInfo';
 import { BadSmellOption, useBadSmellOption } from "@/api/module/badSmellThresholds";
-import { Collapse, Radio } from "antd";
+import useSystemList from '@/store/global-cache-state/useSystemList';
+import { storage } from '@/store/storage/sessionStorage';
+import { Button, Collapse, Form, notification, Radio } from "antd";
+import { useForm } from 'antd/lib/form/Form';
+import { Store } from 'antd/lib/form/interface';
 import React from "react";
 import styles from "./BadSmellThreshold.less";
 import BadSmellThresholdTable from "./components/BadSmellThresholdTable";
 
 const BadSmellThreshold = () => {
+  const [form] = useForm();
+  const [systemInfoList] = useSystemList();
+  const currentSystemId = Number(storage.getSystemId());
+  const currentSystemInfo = systemInfoList?.value.find(systemInfo => systemInfo.id === currentSystemId);
+
   const renderRadio = (badSmellOption: BadSmellOption) => (
     <Radio
-      checked={badSmellOption.isSelected}
+      value={badSmellOption.id}
+      defaultChecked={badSmellOption.id === currentSystemInfo.badSmellThresholdSuiteId}
       onClick={(e) => e.stopPropagation()}
-      disabled={!badSmellOption.isSelected}
     >
       选择
     </Radio>
@@ -17,26 +27,67 @@ const BadSmellThreshold = () => {
 
   const { data } = useBadSmellOption();
 
+  const tailLayout = {
+    wrapperCol: { offset: 20, span: 16 },
+  };
+
+  const onFinish = (values: Store) => {
+    if (values.badSmellThresholdSuiteId) {
+      currentSystemInfo.badSmellThresholdSuiteId = values.badSmellThresholdSuiteId;
+    }
+    updateSystemInfo(currentSystemInfo).then(() => {
+      notification.success({
+        type: "success",
+        message: "系统信息修改成功！",
+      });
+    });
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
+  const onChange = (e) => {
+    console.log("toto: ", e.target);
+  }
+
   return (
-    <div className={styles.BadSmellThreshold}>
-      <div>请选择合适您系统的指标阈值：</div>
-      <Collapse accordion ghost>
-        {data &&
-          data.map((badSmellOption) => {
-            if (!badSmellOption) return null;
-            return (
-              <Collapse.Panel
-                className={styles.collapseItem}
-                header={badSmellOption.suiteName}
-                extra={renderRadio(badSmellOption)}
-                key={badSmellOption.id}
-              >
-                <BadSmellThresholdTable data={badSmellOption.thresholds}></BadSmellThresholdTable>
-              </Collapse.Panel>
-            );
-          })}
-      </Collapse>
-    </div>
+    <Form
+      form={form}
+      onFinish={onFinish}
+    >
+      <div className={styles.BadSmellThreshold}>
+        <div>请选择合适您系统的指标阈值：</div>
+        <Form.Item name="badSmellThresholdSuiteId">
+          <Radio.Group className={styles.badSmellThresholdSuiteId} onChange={onChange}>
+            <Collapse accordion ghost>
+              {data &&
+                data.map((badSmellOption) => {
+                  if (!badSmellOption) return null;
+                  return (
+                    <Collapse.Panel
+                      className={styles.collapseItem}
+                      header={badSmellOption.suiteName}
+                      extra={renderRadio(badSmellOption)}
+                      key={badSmellOption.id}
+                    >
+                      <BadSmellThresholdTable data={badSmellOption.thresholds}></BadSmellThresholdTable>
+                    </Collapse.Panel>
+                  );
+                })}
+            </Collapse>
+          </Radio.Group>
+        </Form.Item>
+      </div>
+      <Form.Item {...tailLayout}>
+        <Button htmlType="button" onClick={onReset}>
+          重置
+          </Button>
+        <Button type="primary" htmlType="submit">
+          确定
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
 
