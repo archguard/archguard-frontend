e50675907e706823b493c7b9e347348cf5d3598f
Xiaolin <xlbian@thoughtworks.com>
2020-11-11 12:12:42 +0800

    #569 添加filter，step1.3, 过大的方法tab点击查询按钮，发送请求到后端，得到数据并且正确显示

--- dependence_visual/src/pages/systemEvaluation/SizingEvaluation/SizingEvaluation.tsx
+++ dependence_visual/src/pages/systemEvaluation/SizingEvaluation/SizingEvaluation.tsx
@@ -1,24 +1,62 @@
-import React from "react";
-import { Tabs } from "antd";
+import React, { useState } from "react";
+import { Button, Form, Input, Tabs } from "antd";
 import IssuesList from "@/components/Business/IssuesList/IssuesList";
 import { SizingEvaluationIssuesConfigs } from "./SizingEvaluationIssuesList.config";
 
 const SizingEvaluation = () => {
+  const [parameter, setParameter] = useState({});
+
+  const onFinish = (values: any) => {
+    console.log('finish data values: ', values);
+    setParameter(values);
+  }
+
   return (
-    <Tabs onChange={() => {}} type="card">
-      <Tabs.TabPane tab="过大的方法" key="method">
-        <IssuesList issuesConfig={SizingEvaluationIssuesConfigs.METHOD} />
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="过大的类" key="class">
-        <IssuesList issuesConfig={SizingEvaluationIssuesConfigs.CLASS} />
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="过大的包" key="package">
-        <IssuesList issuesConfig={SizingEvaluationIssuesConfigs.PACKAGE} />
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="过大的模块" key="module">
-        <IssuesList issuesConfig={SizingEvaluationIssuesConfigs.MODULE} />
-      </Tabs.TabPane>
-    </Tabs>
+    <div>
+      <Form
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="module"
+        >
+          <Input placeholder="请输入模块关键字" />
+        </Form.Item>
+        <Form.Item
+          name="packageName"
+        >
+          <Input placeholder="请输入包关键字" />
+        </Form.Item>
+        <Form.Item
+          name="className"
+        >
+          <Input placeholder="请输入类关键字" />
+        </Form.Item>
+        <Form.Item
+          name="name"
+        >
+          <Input placeholder="请输入方法关键字" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            查询
+        </Button>
+        </Form.Item>
+      </Form>
+      <Tabs onChange={() => { }} type="card">
+        <Tabs.TabPane tab="过大的方法" key="method">
+          <IssuesList issuesConfig={SizingEvaluationIssuesConfigs.METHOD} parameter={parameter} />
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="过大的类" key="class">
+          <IssuesList issuesConfig={SizingEvaluationIssuesConfigs.CLASS} />
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="过大的包" key="package">
+          <IssuesList issuesConfig={SizingEvaluationIssuesConfigs.PACKAGE} />
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="过大的模块" key="module">
+          <IssuesList issuesConfig={SizingEvaluationIssuesConfigs.MODULE} />
+        </Tabs.TabPane>
+      </Tabs>
+    </div>
   );
 };
 
