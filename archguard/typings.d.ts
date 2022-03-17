4a11e1152032093e95d017eb5273bb4d40748979
Phodal Huang <h@phodal.com>
2022-03-17 13:19:57 +0800

    fix: update tslib version to fix g6 issue

--- archguard/typings.d.ts
+++ archguard/typings.d.ts
@@ -3,3 +3,9 @@ declare module "*.png";
 declare module '*.less';
 declare module 'react-highlight-words';
 declare module 'markdown-navbar';
+
+// https://github.com/antvis/G6/issues/2806
+declare module '@antv/g6/dist/g6.min.js' {
+  const content: typeof import('@antv/g6')
+  export default content
+}
