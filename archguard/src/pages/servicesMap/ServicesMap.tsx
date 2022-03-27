784bfc657251c7d293f66fe082a5459bf9714a7e
Phodal Huang <h@phodal.com>
2022-03-27 11:39:05 +0800

    docs: add tips for services map

--- archguard/src/pages/servicesMap/ServicesMap.tsx
+++ archguard/src/pages/servicesMap/ServicesMap.tsx
@@ -3,6 +3,7 @@ import ServicesMapMapping from "@/pages/servicesMap/ServicesMapMapping";
 
 function ServicesMap() {
   return (<div>
+    <p>说明：前端当前支持 Axios、UMI-Request，后端支持 Java/Kotlin + Spring、C# + .Net</p>
     <ServicesMapMapping />
   </div>)
 }
