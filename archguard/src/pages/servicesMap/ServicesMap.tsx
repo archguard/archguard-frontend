97dc8b959e351926d609d5d47fede76c89834e98
Phodal Huang <h@phodal.com>
2022-03-27 11:45:12 +0800

    docs: add tips for language

--- archguard/src/pages/servicesMap/ServicesMap.tsx
+++ archguard/src/pages/servicesMap/ServicesMap.tsx
@@ -4,6 +4,7 @@ import ServicesMapMapping from "@/pages/servicesMap/ServicesMapMapping";
 function ServicesMap() {
   return (<div>
     <p>说明：前端当前支持 Axios、UMI-Request，后端支持 Java/Kotlin + Spring、C# + .Net</p>
+    <p>新的语言和框架支持，请移步：<a href="https://github.com/archguard/scanner" target={"_blank"}>https://github.com/archguard/scanner</a></p>
     <ServicesMapMapping />
   </div>)
 }
