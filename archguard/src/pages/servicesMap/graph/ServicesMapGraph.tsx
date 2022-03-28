361370764c62c4415a8f38b3a25b0374a5608386
Phodal Huang <h@phodal.com>
2022-03-28 18:32:30 +0800

    feat: make line in colorful

--- archguard/src/pages/servicesMap/graph/ServicesMapGraph.tsx
+++ archguard/src/pages/servicesMap/graph/ServicesMapGraph.tsx
@@ -15,15 +15,10 @@ export enum FETCH_STATUS {
   NOT_INITIATED = 'not_initiated',
 }
 
-export const getCytoscapeDivStyle = (
-  status: FETCH_STATUS
-): CSSProperties => ({
-  background: `linear-gradient(90deg, #1a1b20 calc(24px - calc($4px/ 2)), transparent 1%)
-center,
-linear-gradient(#fafbfd calc(24px - calc($4px/ 2)))), transparent 1%)
-center, #d3dae6`,
-  backgroundSize: `24px 4px}`,
-  cursor: `${ status === FETCH_STATUS.LOADING ? 'wait' : 'grab' }`,
+export const getCytoscapeDivStyle = (): CSSProperties => ({
+  background: `linear-gradient(90deg, #fafbfd 20px, transparent 20%) center, linear-gradient(#fafbfd 20px, transparent 20%) center, #d3dae6`,
+  backgroundSize: `24px 24px`,
+  cursor: 'grab',
   marginTop: 0,
 });
 
@@ -37,7 +32,6 @@ interface ServicesMapGraphProps {
 
 function ServicesMapGraph(props: ServicesMapGraphProps) {
   const heightWithPadding = 800;
-  const status = "success";
   const [elements, setElements] = useState(null)
 
   useEffect(() => {
@@ -57,7 +51,7 @@ function ServicesMapGraph(props: ServicesMapGraphProps) {
           elements={ elements }
           height={ heightWithPadding }
           serviceName={ "Services Map" }
-          style={ getCytoscapeDivStyle(status) }>
+          style={ getCytoscapeDivStyle() }>
           { "Services Map" }
         </CytoscapeComponent>
       }
