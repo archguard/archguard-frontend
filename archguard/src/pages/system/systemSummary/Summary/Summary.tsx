5301bc581022c2786d20f44bea7833fd95b9bfe3
Phodal Huang <h@phodal.com>
2022-03-30 20:45:07 +0800

    fix: fix lint

--- archguard/src/pages/system/systemSummary/Summary/Summary.tsx
+++ archguard/src/pages/system/systemSummary/Summary/Summary.tsx
@@ -18,7 +18,7 @@ import ApiResourceTree from "@/pages/system/systemSummary/Summary/components/Api
 function Summary() {
   // todo: load system id from url
   const { data: overviewCount } = useOverviewCount();
-  const [services, setServices] = useState( {} as any);
+  const [services, setServices] = useState({} as any);
   const [unstableFiles, setUnstableFiles] = useState([]);
 
   const [systemList] = useSystemList();
@@ -48,96 +48,97 @@ function Summary() {
   }, []);
 
   const demandColumns = [
-    {title: 'Source Method', dataIndex: 'sourceMethod', key: 'sourceMethod',},
-    {title: 'URI', dataIndex: 'targetUrl', key: 'targetUrl',},
-    {title: 'HTTP Method', dataIndex: 'targetHttpMethod', key: 'targetHttpMethod',},
+    { title: 'Source Method', dataIndex: 'sourceMethod', key: 'sourceMethod', },
+    { title: 'URI', dataIndex: 'targetUrl', key: 'targetUrl', },
+    { title: 'HTTP Method', dataIndex: 'targetHttpMethod', key: 'targetHttpMethod', },
   ];
 
   const resourceColumns = [
-    {title: 'Package Name', dataIndex: 'packageName', key: 'packageName',},
-    {title: 'className', dataIndex: 'className', key: 'className',},
-    {title: 'methodName', dataIndex: 'methodName', key: 'methodName',},
-    {title: 'sourceHttpMethod', dataIndex: 'sourceHttpMethod', key: 'sourceHttpMethod',},
-    {title: 'sourceUrl', dataIndex: 'sourceUrl', key: 'sourceUrl',},
+    { title: 'Package Name', dataIndex: 'packageName', key: 'packageName', },
+    { title: 'className', dataIndex: 'className', key: 'className', },
+    { title: 'methodName', dataIndex: 'methodName', key: 'methodName', },
+    { title: 'sourceHttpMethod', dataIndex: 'sourceHttpMethod', key: 'sourceHttpMethod', },
+    { title: 'sourceUrl', dataIndex: 'sourceUrl', key: 'sourceUrl', },
   ];
 
   const lineCountColumns = [
-    {title: '语言', dataIndex: 'language', key: 'language',},
-    {title: '行数', dataIndex: 'lineCount', key: 'lineCount',},
-    {title: '文件数', dataIndex: 'fileCount', key: 'fileCount',},
+    { title: '语言', dataIndex: 'language', key: 'language', },
+    { title: '行数', dataIndex: 'lineCount', key: 'lineCount', },
+    { title: '文件数', dataIndex: 'fileCount', key: 'fileCount', },
   ];
 
   const unstableColumns = [
-    {title: '路径', dataIndex: 'path', key: 'path',},
-    {title: '变更', dataIndex: 'changes', key: 'changes',},
-    {title: '行数', dataIndex: 'lineCount', key: 'lineCount',},
+    { title: '路径', dataIndex: 'path', key: 'path', },
+    { title: '变更', dataIndex: 'changes', key: 'changes', },
+    { title: '行数', dataIndex: 'lineCount', key: 'lineCount', },
   ];
 
   return (
     <div>
-      <div className={styles.header}>
-        <div className={styles.title}>
-          <div className={styles.name}>{systemName}</div>
+      <div className={ styles.header }>
+        <div className={ styles.title }>
+          <div className={ styles.name }>{ systemName }</div>
         </div>
-        <BaButton onClick={() => history.push(`/${storage.getSystemId()}/systemEvolving/MeasureIndicators`)}>
+        <BaButton onClick={ () => history.push(`/${ storage.getSystemId() }/systemEvolving/MeasureIndicators`) }>
           查看指标看板
         </BaButton>
       </div>
 
-      <div className={styles.body}>
-        <div className={styles.detail}>
-          <div className={styles.overview}>
-            <BaLabel value={overviewCount?.repoCount} text="代码仓数"></BaLabel>
-            <BaLabel value={overviewCount?.moduleCount} text="模块数"></BaLabel>
-            <BaLabel value={overviewCount?.contributorCount} text="代码贡献人数"></BaLabel>
-            <BuGrade text="架构质量等级" grade={overviewCount?.qualityLevel}></BuGrade>
+      <div className={ styles.body }>
+        <div className={ styles.detail }>
+          <div className={ styles.overview }>
+            <BaLabel value={ overviewCount?.repoCount } text="代码仓数"></BaLabel>
+            <BaLabel value={ overviewCount?.moduleCount } text="模块数"></BaLabel>
+            <BaLabel value={ overviewCount?.contributorCount } text="代码贡献人数"></BaLabel>
+            <BuGrade text="架构质量等级" grade={ overviewCount?.qualityLevel }></BuGrade>
           </div>
         </div>
-        <div className={styles.changes}>
-          <Table className={styles.codeChart} dataSource={overviewCount?.lineCounts} columns={lineCountColumns} size={'middle'} pagination={
+        <div className={ styles.changes }>
+          <Table className={ styles.codeChart } dataSource={ overviewCount?.lineCounts } columns={ lineCountColumns }
+                 size={ 'middle' } pagination={
             { defaultPageSize: 5 }
           }/>
           <DonutChart
-            data={overviewCount?.lineCounts || []}
+            data={ overviewCount?.lineCounts || [] }
             autoFit
-            height={350}
-            width={600}
-            radius={0.8}
+            height={ 350 }
+            width={ 600 }
+            radius={ 0.8 }
             padding="auto"
             angleField="lineCount"
             colorField="language"
-            pieStyle={{ stroke: "white", lineWidth: 5 }}
+            pieStyle={ { stroke: "white", lineWidth: 5 } }
           />
         </div>
       </div>
       <h2>不稳定性</h2>
-      <div className={styles.physical}>
-        <div className={styles.changes}>
-          <div className={styles.graph}>
+      <div className={ styles.physical }>
+        <div className={ styles.changes }>
+          <div className={ styles.graph }>
             <h2>提交变更频率（大小）</h2>
             <FileSizing/>
           </div>
-          <div className={styles.graph}>
+          <div className={ styles.graph }>
             <h2>提交变更频率（大小）-文件长度（颜色深浅）</h2>
             <FileChangeSizing/>
           </div>
         </div>
         <div>
           <h2>不稳定文件（Top 50 行数 + Top 50 变更）</h2>
-          <Table dataSource={unstableFiles} columns={unstableColumns} pagination={
+          <Table dataSource={ unstableFiles } columns={ unstableColumns } pagination={
             { defaultPageSize: 5 }
           }/>
         </div>
       </div>
-      <div className={styles.physical}>
-        <div className={styles.demand}>
-          <h2>API 使用清单 ({services["demands"]?.length})</h2>
-          <Table dataSource={services["demands"]} columns={demandColumns}/>
+      <div className={ styles.physical }>
+        <div className={ styles.demand }>
+          <h2>API 使用清单 ({ services["demands"]?.length })</h2>
+          <Table dataSource={ services["demands"] } columns={ demandColumns }/>
         </div>
-        <div className={styles.resource}>
-          <h2>API 提供清单 ({services["resources"]?.length})</h2>
-          <Table dataSource={services["resources"]} columns={resourceColumns}/>
-          <ApiResourceTree dataSource={services["resources"]} />
+        <div className={ styles.resource }>
+          <h2>API 提供清单 ({ services["resources"]?.length })</h2>
+          <Table dataSource={ services["resources"] } columns={ resourceColumns }/>
+          <ApiResourceTree dataSource={ services["resources"] }/>
         </div>
       </div>
     </div>
