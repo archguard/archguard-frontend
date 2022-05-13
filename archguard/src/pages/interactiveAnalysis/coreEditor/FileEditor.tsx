e76c05eb3b677b46ebfe0ed15501f86cd20493f4
Phodal Huang <h@phodal.com>
2022-05-13 09:22:13 +0800

    feat(notebook): try to use nteract/messaging but not working with umd

--- archguard/src/pages/interactiveAnalysis/coreEditor/FileEditor.tsx
+++ archguard/src/pages/interactiveAnalysis/coreEditor/FileEditor.tsx
@@ -1,7 +1,11 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import BlockEditor from "@/pages/interactiveAnalysis/coreEditor/BlockEditor";
+import { createMessage } from "@nteract/messaging";
+import jmp from "jmp";
+import { createMainChannel } from "enchannel-zmq-backend";
 
 function FileEditor() {
+  const [channels, setChannels] = useState(null)
   const all = `
 # 架构
 
@@ -17,6 +21,33 @@ var layer = layered {
 \`\`\`
   `
 
+  const config = {
+    "control_port": 50160,
+    "shell_port": 57503,
+    "transport": "tcp",
+    "signature_scheme": "hmac-sha256",
+    "stdin_port": 52597,
+    "hb_port": 42540,
+    "ip": "127.0.0.1",
+    "iopub_port": 40885,
+    "key": "a0436f6c-1916-498b-8eb9-e81ab9368e84"
+  }
+  useEffect(() => {
+    createChannel()
+  }, [setChannels])
+
+  const createChannel = async () => {
+    const mainChannel = await createMainChannel(
+      config,
+      undefined,
+      undefined,
+      undefined,
+      jmp
+    );
+
+    setChannels(mainChannel)
+  }
+
   const testcode = `@file:DependsOn("org.archguard.scanner:doc-executor:2.0.0-alpha.2")
 
 import org.archguard.dsl.*
@@ -29,7 +60,13 @@ var layer = layered {
   // todo: parse markdown to dispatch block and graph
 
   const runCode = useCallback((code) => {
-    console.log(code)
+    console.log(channels)
+
+    const message = createMessage("inspect_request", {
+      code: "string.for",
+      cursor_pos: 10,
+      detail_level: 1
+    });
   });
 
 
---
+++ archguard/src/pages/interactiveAnalysis/message/message.ts
@@ -0,0 +1,3 @@
+// for more documents: see in: [Messaging in Jupyter](https://jupyter-client.readthedocs.io/en/stable/messaging.html)
+
+
