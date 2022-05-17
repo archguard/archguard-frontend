1e693afd22378f9d30b065298f24a6ca2b713190
Phodal Huang <h@phodal.com>
2022-05-17 21:57:30 +0800

    docs: update sample codes

--- archguard/src/pages/interactiveAnalysis/coreEditor/CoreEditor.tsx
+++ archguard/src/pages/interactiveAnalysis/coreEditor/CoreEditor.tsx
@@ -64,6 +64,8 @@ function CoreEditor() {
   const value = `
 ## Backend DSL
 
+目标：基于 ArchGuard Backend，提供 CRUD 封装的 API，如构建系统，查询依赖关系等。
+
 \`\`\`kotlin
 repos {
     repo(name = "Backend", language = "Kotlin", scmUrl = "https://github.com/archguard/archguard")
@@ -72,6 +74,8 @@ repos {
 \`\`\`
 
 ## 架构 DSL
+
+目标：设计系统架构，可视化架构设计等，生成系统的架构 DSL。
 
 \`\`\`kotlin
 @file:DependsOn("org.archguard.scanner:doc-executor:2.0.0-alpha.6")
@@ -94,8 +98,10 @@ graph().show(layer.relations())
 
 ## Scanner DSL
 
+目标：结合 ArchGuard Scanner 中的能力，对系统进行 Scanner、Analyser、Linter 等。
+
 \`\`\`kotlin
-linter('Backend').layer()
+linter("Backend").layer()
 \`\`\`
 
   `
