fc791a11434dcecebad8dac609c7b5f48f691030
Phodal Huang <h@phodal.com>
2022-06-08 17:46:59 +0800

    refactor: split issue columns

---
+++ archguard/src/pages/system/systemSummary/Summary/columns/issueColumns.tsx
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+import {
+  ClockCircleOutlined,
+  DownCircleOutlined,
+  ExclamationCircleOutlined,
+  UpCircleOutlined
+} from "@ant-design/icons";
+import { IssuePosition } from "@/api/module/issue";
+
+export const breakRender = (text, record) => (
+  <div style={{ wordWrap: 'break-word', wordBreak: 'break-word' }}>
+    {text}
+  </div>
+);
+
+export const issueColumns = [
+  { title: "name", dataIndex: "name", key: "name" },
+  { title: "detail", dataIndex: "detail", key: "detail", width: 300, render: breakRender },
+  { title: "fullName", dataIndex: "fullName", key: "fullName", width: 200, render: breakRender },
+  { title: "ruleId", dataIndex: "ruleId", key: "ruleId", width: 300, render: breakRender },
+  {
+    title: "ruleType",
+    dataIndex: "ruleType",
+    key: "ruleType",
+    width: 200,
+    filters: [
+      {
+        text: "TEST_CODE_SMELL",
+        value: "TEST_CODE_SMELL",
+      },
+      {
+        text: "HTTP_API_SMELL",
+        value: "HTTP_API_SMELL",
+      },
+      {
+        text: "SQL_SMELL",
+        value: "SQL_SMELL",
+      },
+      {
+        text: "LAYER",
+        value: "LAYER",
+      },
+    ],
+    onFilter: (value: string, record) => record.ruleType.indexOf(value) === 0,
+    sorter: (a, b) => a.ruleType.length - b.ruleType.length,
+    render: breakRender,
+  },
+  {
+    title: "severity",
+    dataIndex: "severity",
+    width: 50,
+    key: "severity",
+    filters: [
+      {
+        text: "HINT",
+        value: "HINT",
+      },
+      {
+        text: "WARN",
+        value: "WARN",
+      },
+      {
+        text: "INFO",
+        value: "INFO",
+      },
+      {
+        text: "BLOCKER",
+        value: "BLOCKER",
+      },
+    ],
+    onFilter: (value: string, record) => record.severity.indexOf(value) === 0,
+    sorter: (a, b) => a.severity.length - b.severity.length,
+    render: (text, record) => {
+      switch (text) {
+        case "HINT":
+          return <ClockCircleOutlined style={{ fontSize: "24px", color: "#08c" }} />;
+        case "WARN":
+          return <UpCircleOutlined style={{ fontSize: "24px", color: "#c0c0c0" }} />;
+        case "INFO":
+          return <DownCircleOutlined style={{ fontSize: "24px", color: "#0f0" }} />;
+        case "BLOCKER":
+          return <ExclamationCircleOutlined style={{ fontSize: "24px", color: "#f00" }} />;
+        default:
+          return "";
+      }
+    },
+  },
+  { title: "source", dataIndex: "source", key: "source", render: breakRender },
+  {
+    title: "position",
+    dataIndex: "position",
+    key: "position",
+    width: 50,
+    render: (text, record) => {
+      if (!text || text == "{}") return "";
+
+      let pos: IssuePosition = JSON.parse(text);
+      return `${pos.startLine}:${pos.startColumn}-${pos.endLine}:${pos.endColumn}`;
+    },
+  },
+];
+
