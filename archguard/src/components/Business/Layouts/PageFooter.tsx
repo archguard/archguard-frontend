8e8d9d3162f08f15408d9a8cb104cea38d0825e7
Phodal Huang <h@phodal.com>
2022-06-13 20:57:07 +0800

    docs: fix license

--- archguard/src/components/Business/Layouts/PageFooter.tsx
+++ archguard/src/components/Business/Layouts/PageFooter.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function PageFooter() {
   return (
     <div style={{ textAlign: "center"}}>
-      <span>ArchGuard ©2020 Created by ThoughtWorks</span>
+      <span>ArchGuard ©2020~ Created in Thoughtworks</span>
     </div>
   );
 }
