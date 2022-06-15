c3f3e95ad98f5b1ff54eb1d841f806142a2ac386
Phodal Huang <h@phodal.com>
2022-06-15 11:23:37 +0800

    docs: add link to message map

--- archguard/src/pages/messageMap/MessageMap.tsx
+++ archguard/src/pages/messageMap/MessageMap.tsx
@@ -4,6 +4,8 @@ export default function MessageMap() {
   return (
     <div>
       <h2>Coming soon..</h2>
+      For more see in:
+      <a target={"_blank"} href="https://github.com/archguard/archguard/issues/34">https://github.com/archguard/archguard/issues/34</a>
     </div>
   )
 }
