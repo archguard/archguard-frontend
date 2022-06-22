bee32172e98f2e6715ccfb4bc20874e0b7916f5e
Phodal Huang <h@phodal.com>
2022-06-22 16:36:58 +0800

    refactor(insights): add basci cards render #12

--- archguard/src/pages/insights/Insights.tsx
+++ archguard/src/pages/insights/Insights.tsx
@@ -4,33 +4,38 @@ import useSystemList from "@/store/global-cache-state/useSystemList";
 import SmartSuggest from "@/pages/insights/searchbar/SmartSuggest";
 import "./Insights.less";
 import { scaInsight } from "@/api/insights/scaInsight";
+import { BaCard } from "@/components/Basic/Card/Card";
 
 function Insights() {
   const { Option } = Select;
   const [systemInfo] = useSystemList();
   const [systemId, setSystemId] = useState(-1);
-  const [result, setResult] = useState(null as any)
-  const [searchText, setSearchText] = useState("")
+  const [result, setResult] = useState(null as any);
+  const [searchText, setSearchText] = useState("");
+  const [cards, setCards] = useState([]);
 
   const onChange = (value: string) => {};
 
-  const onFinish = useCallback(
-    (values: any) => {
-      console.log(searchText);
-      scaInsight({ systemId: systemId, expression: searchText }).then((data) => {
-        setResult(data);
-      });
+  const onFinish = useCallback(() => {
+    scaInsight({ systemId: systemId, expression: searchText }).then((data) => {
+      setResult(data);
+      setCards((prevCards) => [...prevCards, data]);
+    });
+  }, [searchText, systemId, setCards]);
+
+  const onSystemChange = useCallback(
+    (value) => {
+      setSystemId(value);
     },
-    [searchText, systemId],
+    [setSystemId],
   );
 
-  const onSystemChange = useCallback((value) => {
-    setSystemId(value)
-  }, [setSystemId]);
-
-  const changeSearchInput = useCallback((text: string) => {
-    setSearchText(text)
-  }, [setSearchText]);
+  const changeSearchInput = useCallback(
+    (text: string) => {
+      setSearchText(text);
+    },
+    [setSearchText],
+  );
 
   return (
     <div>
@@ -40,23 +45,23 @@ function Insights() {
           wrapperCol={{ span: 14 }}
           labelCol={{ span: 6 }}
           onFinish={onFinish}
-          layout="inline">
-
+          layout="inline"
+        >
           <Select
             showSearch
             placeholder="Select a System"
             defaultActiveFirstOption={true}
-            onChange={ (value) => onSystemChange(value) }>
-
-            { systemInfo?.value!.map((system, index) => (
+            onChange={(value) => onSystemChange(value)}
+          >
+            {systemInfo?.value!.map((system, index) => (
               <Select.Option
-                disabled={ system.scanned !== "SCANNED" }
-                value={ system.id }
-                key={ `${ system.systemName }_${ index }` }
+                disabled={system.scanned !== "SCANNED"}
+                value={system.id}
+                key={`${system.systemName}_${index}`}
               >
-                { system.systemName }
+                {system.systemName}
               </Select.Option>
-            )) }
+            ))}
           </Select>
 
           <Select
@@ -64,24 +69,31 @@ function Insights() {
             showSearch
             placeholder="Select a type"
             defaultActiveFirstOption={true}
-            onChange={onChange}>
-
+            onChange={onChange}
+          >
             <Option value="sca">Package Dependencies (Gradle/NPM)</Option>
             <Option value="sourcecode">Source Code</Option>
             <Option value="api">API</Option>
           </Select>
 
-          <div style={{ height: "32px", width: "800px"}}>
-            <SmartSuggest onChange={changeSearchInput}/>
+          <div style={{ height: "32px", width: "800px" }}>
+            <SmartSuggest onChange={changeSearchInput} />
           </div>
 
           <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
-            <Button type="primary" htmlType="submit">Query</Button>
+            <Button type="primary" htmlType="submit">
+              Query
+            </Button>
           </Form.Item>
         </Form>
       </div>
-
-      { JSON.stringify(result) }
+      <div className="result-container">
+        {JSON.stringify(result)}
+        {cards?.map((card, i) => {
+          console.log(card);
+          return <BaCard>{card.length}</BaCard>;
+        })}
+      </div>
     </div>
   );
 }
