a5929de0fed112064e36ba3640f274b29b88c2aa
Phodal Huang <h@phodal.com>
2022-06-23 11:20:42 +0800

    feat(insights): add histories for support #12

--- archguard/src/pages/insights/Insights.tsx
+++ archguard/src/pages/insights/Insights.tsx
@@ -5,11 +5,14 @@ import SmartSuggest from "@/pages/insights/searchbar/SmartSuggest";
 import "./Insights.less";
 import { customInsight, listInsights, scaInsight } from "@/api/insights/scaInsight";
 import {
+  badSmellWording,
   ChartItem,
   INDICATOR_LEVEL_COLOR
 } from "@/pages/system/systemEvolving/MeasureIndicators/Components/ChartCard";
 import { JsonView } from "@/pages/interactiveAnalysis/block/components/JsonView";
 import { BaCard } from "@/components/Basic/Card/Card";
+import { groupBy } from "lodash";
+import { BaTipsIcon } from "@/components/Basic/TipsIcon/TipsIcon";
 
 function Insights() {
   const { Option } = Select;
@@ -18,6 +21,7 @@ function Insights() {
   const [result, setResult] = useState(null as any);
   const [searchText, setSearchText] = useState("");
   const [cards, setCards] = useState([]);
+  const [histories, setHistories] = useState({ });
 
   const onChange = (value: string) => {};
 
@@ -52,9 +56,9 @@ function Insights() {
 
   useEffect(() => {
     listInsights().then((data) => {
-      console.log(data)
+      setHistories(groupBy(data, "name"))
     });
-  })
+  }, [setHistories])
 
   function createResult(card, i: number) {
     const graphData = [
@@ -142,6 +146,17 @@ function Insights() {
           </Form.Item>
         </Form>
       </div>
+      <div className="history-container">
+        {Object.keys(histories).map((key, i) => {
+          return <div key={"insight-history" + i} className="insight-result">
+            <BaCard className="insight-chart">
+              <div>{key}</div>
+              <ChartItem color={INDICATOR_LEVEL_COLOR.pass} graphData={histories[key]} />
+            </BaCard>
+          </div>;
+        })}
+      </div>
+
       <div className="result-container">
         {cards?.map((card, i) => {
           return createResult(card, i);
