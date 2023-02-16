06bbf6814e40fed6d840975fedae0702ecd20e2f
Phodal Huang <h@phodal.com>
2023-02-16 09:33:40 +0800

    fix: fix uid

--- archguard/src/pages/system/systemSummary/Summary/Summary.tsx
+++ archguard/src/pages/system/systemSummary/Summary/Summary.tsx
@@ -73,70 +73,87 @@ function Summary() {
   function toggleFileSizing() {setShowFileSizing(!showFileSizing)}
   function toggleFileChangeSizing() {setShowFileChangeSizing(!showFileChangeSizing)}
 
-  return (
-    <div>
-      <div className="header">
-        <div className="title">
-          <div className="name">{ systemName }</div>
-        </div>
-        <BaButton onClick={ () => history.push(`/${ storage.getSystemId() }/systemEvolving/MeasureIndicators`) }>
-          { formatMessage({ id: 'SYSTEM_OVERVIEW.VIEW_METRIC_DASHBOARD' }) }
-        </BaButton>
+  return <div>
+    <div className="header">
+      <div className="title">
+        <div className="name">{systemName}</div>
       </div>
-      <div className="body">
-        <div className="detail">
-          <div className="overview">
-            <BaLabel value={ overviewCount?.contributorCount }
-                     text={ formatMessage({ id: 'SYSTEM_OVERVIEW.CONTRIBUTORS' }) }/>
-            <BuGrade text={ formatMessage({ id: 'SYSTEM_OVERVIEW.ARCH_LEVEL' }) }
-                     grade={ overviewCount?.qualityLevel }/>
-          </div>
-        </div>
-        <div className="changes">
-          <Table className="code-chart" dataSource={ overviewCount?.lineCounts } columns={ lineCountColumns }
-                 size={ 'middle' } pagination={
-            { defaultPageSize: 5 }
-          }/>
-          { overviewCount?.lineCounts && <LineCountChart  dataSource={ overviewCount?.lineCounts } /> }
+      <BaButton
+        onClick={() => history.push(`/${storage.getSystemId()}/systemEvolving/MeasureIndicators`)}
+      >
+        {formatMessage({ id: "SYSTEM_OVERVIEW.VIEW_METRIC_DASHBOARD" })}
+      </BaButton>
+    </div>
+    <div className="body">
+      <div className="detail">
+        <div className="overview">
+          <BaLabel
+            value={overviewCount?.contributorCount}
+            text={formatMessage({ id: "SYSTEM_OVERVIEW.CONTRIBUTORS" })}
+          />
+          <BuGrade
+            text={formatMessage({ id: "SYSTEM_OVERVIEW.ARCH_LEVEL" })}
+            grade={(overviewCount as any)?.qualityLevel}
+          />
         </div>
       </div>
-      <h2>{ formatMessage({ id: 'SYSTEM_OVERVIEW.STABLE' }) }</h2>
-      <div className="physical">
-        <div className="changes">
-          <div className="graph">
-            <h2>{ formatMessage({ id: 'SYSTEM_OVERVIEW.CHANGE_FREQUENCY_SIZE' }) }</h2>
-            <div>Show: <Switch onChange={toggleFileSizing} /></div>
-            { showFileSizing && <FileSizing systemId={ systemId }/> }
+      <div className="changes">
+        <Table
+          className="code-chart"
+          rowKey={(index) => index}
+          dataSource={overviewCount?.lineCounts}
+          columns={lineCountColumns}
+          size={"middle"}
+          pagination={{ defaultPageSize: 5 }}
+        />
+        {overviewCount?.lineCounts && <LineCountChart dataSource={(overviewCount as any)?.lineCounts} />}
+      </div>
+    </div>
+    <h2>{formatMessage({ id: "SYSTEM_OVERVIEW.STABLE" })}</h2>
+    <div className="physical">
+      <div className="changes">
+        <div className="graph">
+          <h2>{formatMessage({ id: "SYSTEM_OVERVIEW.CHANGE_FREQUENCY_SIZE" })}</h2>
+          <div>
+            Show: <Switch onChange={toggleFileSizing} />
           </div>
-          <div className="graph">
-            <h2>{ formatMessage({ id: 'SYSTEM_OVERVIEW.CHANGE_FREQUENCY_SIZE_LINE_COUNT' }) }</h2>
-            <div>Show: <Switch onChange={toggleFileChangeSizing} /></div>
-            { showFileChangeSizing && <FileChangeSizing systemId={ systemId }/> }
-          </div>
+          {showFileSizing && <FileSizing systemId={systemId} />}
         </div>
-        <div>
-          <h2>{ formatMessage({ id: 'SYSTEM_OVERVIEW.UNSTABLE_TOP_50_FILE' }) }</h2>
-          <Table dataSource={ unstableFiles } columns={ unstableColumns } pagination={
-            { defaultPageSize: 5 }
-          }/>
+        <div className="graph">
+          <h2>{formatMessage({ id: "SYSTEM_OVERVIEW.CHANGE_FREQUENCY_SIZE_LINE_COUNT" })}</h2>
+          <div>
+            Show: <Switch onChange={toggleFileChangeSizing} />
+          </div>
+          {showFileChangeSizing && <FileChangeSizing systemId={systemId} />}
         </div>
       </div>
       <div>
-        <h2>{ formatMessage({ id: 'SYSTEM_OVERVIEW.PROJECT_DEPENDENCY' }) } ({ projectDependency.length })</h2>
-        <div>
-          { stacks && stacks.icons && <div className="stack-icons">{
-            stacks.icons.map((item: string, index: number) =>
-              <div className="stack-item" key={index}>
-                <img src={item.img}  alt={"text"}/>
-                <p>{item.name}</p>
-              </div>)
-          }</div>
-          }
-        </div>
-        <Table dataSource={ projectDependency } columns={ projectDependencyColumns }/>
+        <h2>{formatMessage({ id: "SYSTEM_OVERVIEW.UNSTABLE_TOP_50_FILE" })}</h2>
+        <Table
+          dataSource={unstableFiles}
+          rowKey={(index) => index}
+          columns={unstableColumns}
+          pagination={{ defaultPageSize: 5 }}
+        />
       </div>
     </div>
-  );
+    <div>
+      <h2>
+        {formatMessage({ id: "SYSTEM_OVERVIEW.PROJECT_DEPENDENCY" })} ({projectDependency.length})
+      </h2>
+      <div>
+        {stacks && stacks.icons && <div className="stack-icons">
+            {stacks.icons.map((item: string, index: number) => (
+              <div className="stack-item" key={index}>
+                <img src={item.img} alt={"text"} />
+                <p>{item.name}</p>
+              </div>
+            ))}
+          </div>}
+      </div>
+      <Table dataSource={projectDependency as any} rowKey={(index) => index} columns={projectDependencyColumns} />
+    </div>
+  </div>;
 }
 
 export default Summary;
